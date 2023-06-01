919de297e0ffadd5d143e4873053eac5673a14b1
Code-Choudhary <devkaransingh928@gmail.com>
2023-06-01 15:01:32 +0530

    added new commponent Navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
 
 function App() {
   return (
-<<<<<<< HEAD
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar/>
       </header>
     </div>
-=======
-    <></>
->>>>>>> 453b8953a13ac949034f76a104e6023bf12a48ea
+
   );
 }
 
